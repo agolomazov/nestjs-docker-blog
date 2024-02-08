88c9037b4cb4adfa72e55536e34279eb436c6a7e
Anton Golomazov <golomazov-tosha2012@yandex.ru>
2024-02-08 18:41:33 +0300

    Add support function for create user

--- nest-api/src/user/user.controller.ts
+++ nest-api/src/user/user.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserEntity } from './user.entity';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  async createUser(@Body('user') body: CreateUserDto) {
+  async createUser(@Body('user') body: CreateUserDto): Promise<UserEntity> {
     return await this.userService.createUser(body);
   }
 }
--- nest-api/src/user/user.service.ts
+++ nest-api/src/user/user.service.ts
@@ -1,9 +1,20 @@
 import { CreateUserDto } from './dto/create-user.dto';
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  async createUser(body: CreateUserDto) {
-    return body;
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(body: CreateUserDto): Promise<UserEntity> {
+    const newUser = new UserEntity();
+    Object.assign(newUser, body);
+
+    return await this.userRepository.save(newUser);
   }
 }
