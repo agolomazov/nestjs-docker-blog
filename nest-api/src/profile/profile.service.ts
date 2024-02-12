cf10e08573d1565b10419348aab9b340b7d2dd74
Anton Golomazov <golomazov-tosha2012@yandex.ru>
2024-02-12 20:02:51 +0300

    Complete functions for following profiles

--- nest-api/src/profile/profile.service.ts
+++ nest-api/src/profile/profile.service.ts
@@ -67,6 +67,36 @@ export class ProfileService {
     return { ...user, following: true };
   }
 
+  async unfollowProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    if (currentUserId === user.id) {
+      throw new BadRequestException('Follower and following cant be equal');
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: {
+        followerId: currentUserId,
+        followingId: user.id,
+      },
+    });
+
+    if (follow) {
+      await this.followRepository.remove(follow);
+    }
+
+    return { ...user, following: false };
+  }
+
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
     delete profile.email;
     delete profile.id;
